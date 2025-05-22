f98fc29191f6ce6304337f1cd4e8714ae0d658a6
Johan Durish <jdurish2@gmail.com>
2025-05-22 15:22:05 -0400

    Implement place-, get-, and has- ShipAt methods

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -1,7 +1,20 @@
+import Ship from "./Ship";
 import shipLengths from "./shipLengths";
 
 export default class Gameboard {
+   static size = 10;
+
+   static areValidCoordinates(...coordinates) {
+      return coordinates.every((coordinate) => coordinate >= 0 && coordinate < this.size);
+   }
+
    constructor() {
+      this.board = new Array(this.constructor.size);
+
+      for (let column = 0; column < this.constructor.size; column++) {
+         this.board[column] = new Array(this.size);
+      }
+
       this.ships = []
       this.missedAttacks = [];
       this.hitAttacks = [];
@@ -9,15 +22,54 @@ export default class Gameboard {
    }
 
    placeShipAt(x, y, shipName, orientation) {
+      const shipLength = shipLengths[shipName];
 
+      let deltaX = 0;
+      let deltaY = 0;
+
+      switch (orientation) {
+      case "right":
+         deltaX = 1;
+         break;
+      case "down":
+         deltaY = 1;
+         break;
+      case "left":
+         deltaX = -1;
+         break;
+      case "up":
+         deltaY = -1;
+         break;
+      }
+
+      const endX = x + (shipLength - 1) * deltaX;
+      const endY = y + (shipLength - 1) * deltaY;
+
+      if (!this.constructor.areValidCoordinates(x, y, endX, endY)) {
+         throw new Error("Invalid placement");
+      }
+
+      const ship = new Ship(shipLength);
+      
+      this.ships.push(ship);
+      
+      for (let delta = 0; delta < ship.length; delta++) {
+         this.board[x + delta * deltaX][y + delta * deltaY] = ship;
+      }
+
+      this.unplaced.delete(shipName);
    }
 
    getShipAt(x, y) {
+      if (!this.constructor.areValidCoordinates(x, y)) {
+         throw new Error("Invalid coordinates");
+      }
 
+      return this.board[x][y] ?? null;
    }
 
    hasShipAt(x, y) {
-
+      return this.getShipAt(x, y) !== null;
    }
 
    hasAllShips() {
