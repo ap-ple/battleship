c773767862a70008f71faf8eae2e7a49010aafef
Johan Durish <jdurish2@gmail.com>
2025-05-27 12:36:29 -0400

    Add player name

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,7 +1,9 @@
 import Gameboard from "./Gameboard";
 
 export default class Player {
-   constructor() {
+   constructor(name) {
+      this.name = name;
+
       this.gameboards = {
          ocean: new Gameboard(),
          target: new Gameboard()
