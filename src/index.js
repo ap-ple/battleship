7156208b1c35ff2724c36df0e23c3c97dbec98cc
Johan Durish <jdurish2@gmail.com>
2025-05-30 09:54:05 -0400

    Use random placements for computer and player

--- src/index.js
+++ src/index.js
@@ -8,18 +8,30 @@ import {renderGame, renderMessage} from "./modules/renderer.js";
 const PLAYER = "Player";
 const COMPUTER = "Computer";
 
+const ORIENTATIONS = ["right", "down", "left", "up"];
+
 let game = new Game(PLAYER, COMPUTER);
 
-// TODO: Implement Player and Computer placements
-
-let y = 0;
+// TODO: Implement Player placements
 
 for (const shipName of ships) {
-   for (const player of game.playerList) {
-      player.placeShipAt(0, y, shipName, "right");
-   }
+   for (const player of game.playerList) {      
+      while (player.gameboard.unplaced.has(shipName)) {
+         try {
+            const boardSize = player.gameboard.constructor.size;
+      
+            const x = Math.floor(Math.random() * boardSize);
+            const y = Math.floor(Math.random() * boardSize);
 
-   y++;
+            const orientation = ORIENTATIONS.at(Math.floor(Math.random() * ORIENTATIONS.length));
+      
+            player.placeShipAt(x, y, shipName, orientation);
+         }
+         catch {
+            continue;
+         }
+      }
+   }
 }
 
 const playTurn = (x, y) => {
