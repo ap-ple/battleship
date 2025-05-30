5823b1259febe722f0f5828528dc9655e390ea7a
Johan Durish <jdurish2@gmail.com>
2025-05-30 13:02:21 -0400

    Add reset and start buttons

--- src/index.js
+++ src/index.js
@@ -10,28 +10,30 @@ const COMPUTER = "Computer";
 
 const ORIENTATIONS = ["right", "down", "left", "up"];
 
-let game = new Game(PLAYER, COMPUTER);
-
-// TODO: Implement Player placements
-
-for (const shipName of ships) {
-   for (const player of game.playerList) {      
-      while (player.gameboard.unplaced.has(shipName)) {
-         try {
-            const boardSize = player.gameboard.constructor.size;
-      
-            const x = Math.floor(Math.random() * boardSize);
-            const y = Math.floor(Math.random() * boardSize);
-
-            const orientation = ORIENTATIONS.at(Math.floor(Math.random() * ORIENTATIONS.length));
-      
-            player.placeShipAt(x, y, shipName, orientation);
-         }
-         catch {
-            continue;
+const randomGame = () => {
+   const game = new Game(PLAYER, COMPUTER);
+   
+   for (const shipName of ships) {
+      for (const player of game.playerList) {      
+         while (player.gameboard.unplaced.has(shipName)) {
+            try {
+               const boardSize = player.gameboard.constructor.size;
+         
+               const x = Math.floor(Math.random() * boardSize);
+               const y = Math.floor(Math.random() * boardSize);
+   
+               const orientation = ORIENTATIONS.at(Math.floor(Math.random() * ORIENTATIONS.length));
+         
+               player.placeShipAt(x, y, shipName, orientation);
+            }
+            catch {
+               continue;
+            }
          }
       }
    }
+
+   return game;
 }
 
 const playTurn = (x, y) => {
@@ -68,8 +70,34 @@ const playTurn = (x, y) => {
    }
 }
 
-renderGame(game, {
-   attack: playTurn
-});
+let game;
 
-renderMessage("Game start!");
+const startGame = () => {
+   startButton.disabled = true;
+
+   renderMessage("Game start!")
+
+   renderGame(game, {
+      attack: playTurn
+   });
+}
+
+const resetGame = () => {
+   startButton.disabled = false;
+
+   game = randomGame();
+
+   renderMessage("Click Reset Game for new placement or Start Game to begin!")
+
+   renderGame(game);
+}
+
+const startButton = document.querySelector("#start")
+
+startButton.addEventListener("click", startGame);
+
+const resetButton = document.querySelector("#reset")
+
+resetButton.addEventListener("click", resetGame);
+
+resetGame();
